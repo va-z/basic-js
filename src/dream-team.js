66937d3fccc57dd9c7b067d54461d9b66f8956a9
va-z <vasilizadorojnyuk@gmail.com>
2020-09-13 18:11:47 +0500

    feat: solve dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  else {
+    return members
+      .reduce((letters, str) => {
+        if (typeof str === "string")
+          letters.push(str.match(/\w/i)[0].toUpperCase());
+        return letters;
+      }, [])
+      .sort()
+      .join("");
+  }
 };
